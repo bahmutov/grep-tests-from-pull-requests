0c11e7483c68dedc0e259dd43b65cdc1834f663f
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-02-25 10:42:34 -0500

    feat: add get-pr-tests script (#10)
    * feat: add get-pr-tests script
    
    * add CI command to demo the new script

---
+++ bin/get-pr-tests.js
@@ -0,0 +1,71 @@
+#!/usr/bin/env node
+
+const debug = require('debug')('grep-tests-from-pull-requests')
+const arg = require('arg')
+const {
+  getPullRequestNumber,
+  getPullRequestBody,
+  getPullRequestComments,
+  getTestsToRun,
+} = require('../src/utils')
+
+const args = arg({
+  '--owner': String,
+  '--repo': String,
+  '--pull': Number,
+  '--commit': String,
+
+  // aliases
+  '-o': '--owner',
+  '-r': '--repo',
+  '-p': '--pull',
+  '-c': '--commit',
+})
+debug('args: %o', args)
+
+function checkEnvVariables(env) {
+  if (!env.GITHUB_TOKEN && !env.PERSONAL_GH_TOKEN) {
+    console.error(
+      'Cannot find environment variable GITHUB_TOKEN or PERSONAL_GH_TOKEN',
+    )
+    process.exit(1)
+  }
+}
+
+checkEnvVariables(process.env)
+
+const options = {
+  owner: args['--owner'],
+  repo: args['--repo'],
+  pull: args['--pull'],
+  commit: args['--commit'],
+}
+const envOptions = {
+  token: process.env.GITHUB_TOKEN || process.env.PERSONAL_GH_TOKEN,
+}
+
+getPullRequestNumber(
+  options.owner,
+  options.repo,
+  options.pull,
+  options.commit,
+  envOptions,
+)
+  .then(async (testPullRequestNumber) => {
+    if (isNaN(testPullRequestNumber)) {
+      throw new Error('Could not find the pull request number')
+    }
+
+    options.pull = testPullRequestNumber
+
+    const body = await getPullRequestBody(options, envOptions)
+    const prComments = await getPullRequestComments(options, envOptions)
+    const testsToRun = getTestsToRun(['@log', '@sanity'], body, prComments)
+    console.log('tests to run')
+    console.log(testsToRun)
+  })
+
+  .catch((e) => {
+    console.error(e)
+    process.exit(1)
+  })
